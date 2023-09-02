9b5564eca095e4673c539bd25cced43fdea9e9b7
AishwaryaHR123 <140000342+AishwaryaHR123@users.noreply.github.com>
2023-09-02 23:13:33 +0530

    Inserting Values

---
+++ Insertion.sql
@@ -0,0 +1,112 @@
+insert into airport_info values(1,'Ainsworth','Bangalore',577,-42.5,-35.5,'India',5,7,6,91);
+insert into airport_info values(2,'VC Bird','jaipur',677,-34.5,-67.5,'US',6,8,9,38);
+insert into airport_info values(3,'Antwerp','Rajastan',571,-24,-53.5,'Netherland',4,6,9,355);
+insert into airport_info values(4,'Esenboga ','delhi',891,-23.5,-43.1,'Russia',1,2,3,374);
+insert into airport_info values(5,'CerroMoren','mumbai',181,-42.5,-78.9,'Unites States',6,1,2,375);
+insert into airport_info values(6,'Marce','jammu',123,-90.5,-35.5,'America',4,7,9,880);
+insert into airport_info values(7,'Anaheim ','himalaya',567,-87.3,-98.6,'Indonesia',2,5,6,910);
+insert into airport_info values(8,'Amderma ','chicago',987,-56.7,-56.7,'Australia',5,3,6,915);
+insert into airport_info values(9,'Schiphol','london',783,-45.5,-45.5,'China',6,3,6,115);
+insert into airport_info values(10,'Pattimura','Dallas',098,-34.5,-90.8,'Germany',2,5,8,95);
+insert into airport_info values(11,'Queen','orlando',567,-67.8,-67.5,'France',1,4,6,597);
+insert into airport_info values(12,'Civil','denver',435,-85.6,-32.1,'Belgium',2,5,7,267);
+insert into airport_info values(13,'Pattimura','phoneix',675,-23.4,-76.88,'cuba',3,5,6,591);
+insert into airport_info values(14,'Intl Arpt','New York',324,-67.4,-65.4,'brazil',4,7,9,391);
+insert into airport_info values(15,'Pattimura','Chennai',654,-89.7,-54.33,'algeria',3,5,6,291);
+insert into airport_info values(16,'Anderson','Rome',780,-54.3,-43.2,'cameron',1,5,6,895);
+insert into airport_info values(17,'Aniak','Madison',983,-77.6,-54.6,'egypt',7,6,8,855);
+insert into airport_info values(18,'Schiphol','Cairo',345,-83.3,-98.8,'Bhutan',2,4,8,238);
+insert into airport_info values(19,'Ainsworth','boston',234,-31.2,-45.6,'Greece',3,6,7,57);
+insert into airport_info values(20,'LeAnnapoli','Turkey',786,-76.5,-76.5,'Cannada',4,8,9,767);
+select * from airport_info;
+
+INSERT INTO olympic_info VALUES
+    (1, 'Athletics', 28, 5, 1896, 'Opening Ceremony', 'Athlete1', 100, 'Stadium1', 'Male', 'Sprint'),
+    (2, 'Rowing', 24, 15, 1924, 'Finals', 'Athlete2', 50, 'Rink1', 'Female', 'Figure Skating'),
+    (3, 'Badminton', 32, 7, 1932, 'Semifinals', 'Athlete3', 200, 'Stadium2', 'Male', 'Basketball'),
+    (4, 'Baseball', 20, 10, 1956, 'Quarterfinals', 'Athlete4', 75, 'Rink2', 'Female', 'Skiing'),
+    (5, 'BasketBall', 30, 3, 1964, 'Finals', 'Athlete5', 150, 'Stadium3', 'Male', 'Swimming'),
+    (6, 'Boxing', 28, 20, 1976, 'Opening Ceremony', 'Athlete6', 80, 'Rink3', 'Female', 'Ice Hockey'),
+    (7, 'footBall', 26, 12, 1988, 'Semifinals', 'Athlete7', 300, 'Stadium4', 'Male', 'Gymnastics'),
+    (8, 'handball', 22, 8, 1992, 'Quarterfinals', 'Athlete8', 120, 'Rink4', 'Female', 'Bobsleigh'),
+    (9, 'hockey', 34, 6, 2000, 'Finals', 'Athlete9', 180, 'Stadium5', 'Male', 'Volleyball'),
+    (10, 'judo', 18, 19, 2010, 'Opening Ceremony', 'Athlete10', 90, 'Rink5', 'Female', 'Curling'),
+    (11, 'greco', 36, 9, 2024, 'Semifinals', 'Athlete11', 250, 'Stadium6', 'Male', 'Athletics'),
+    (12, 'Swimming', 16, 4, 1968, 'Quarterfinals', 'Athlete12', 110, 'Rink6', 'Female', 'Speed Skating'),
+    (13, 'Diving', 38, 11, 1984, 'Finals', 'Athlete13', 220, 'Stadium7', 'Male', 'Boxing'),
+    (14, 'Water-Polo', 14, 14, 2002, 'Opening Ceremony', 'Athlete14', 70, 'Rink7', 'Female', 'Luge'),
+    (15, 'softball', 40, 2, 1920, 'Semifinals', 'Athlete15', 270, 'Stadium8', 'Male', 'Wrestling'),
+    (16, 'tennis', 12, 16, 1972, 'Quarterfinals', 'Athlete16', 100, 'Rink8', 'Female', 'Ski Jumping'),
+    (17, 'Volleyball', 42, 8, 2016, 'Finals', 'Athlete17', 190, 'Stadium9', 'Male', 'Table Tennis'),
+    (18, 'beachVolleyball', 10, 18, 1998, 'Opening Ceremony', 'Athlete18', 60, 'Rink9', 'Female', 'Biathlon'),
+    (19, 'BMX', 44, 7, 2008, 'Semifinals', 'Athlete19', 230, 'Stadium10', 'Male', 'Archery'),
+    (20, 'Cycling', 8, 6, 1952, 'Quarterfinals', 'Athlete20', 85, 'Rink10', 'Female', 'Cross-Country Skiing');
+    select * from olympic_info;
+    
+INSERT INTO library_info VALUES
+    (1, 101, 2005, '2023-09-10', '2023-09-08', 5, 4, 3, 'English', 'Aishwarya', '2023-09-01'),
+    (2, 102, 2010, '2023-09-12', '2023-09-11', 3, 5, 5, 'Kannada', 'Anusha', '2023-09-02'),
+    (3, 103, 2015, '2023-09-14', '2023-09-13', 2, 3, 2, 'Hindi', 'Pooja', '2023-09-03'),
+    (4, 104, 2019, '2023-09-16', '2023-09-15', 1, 4, 7, 'Telugu', 'Anupama', '2023-09-04'),
+    (5, 105, 2008, '2023-09-18', '2023-09-17', 4, 3, 4, 'Malyalam', 'Shravya', '2023-09-05'),
+    (6, 106, 2012, '2023-09-20', '2023-09-19', 2, 5, 2, 'Spanish', 'Mounika', '2023-09-06'),
+    (7, 107, 2017, '2023-09-22', '2023-09-21', 3, 4, 6, 'French', 'Paavani', '2023-09-07'),
+    (8, 108, 2021, '2023-09-24', '2023-09-23', 1, 5, 3, 'portuguese', 'Nikshepa', '2023-09-08'),
+    (9, 109, 2006, '2023-09-26', '2023-09-25', 4, 3, 5, 'English', 'Punith', '2023-09-09'),
+    (10, 110, 2011, '2023-09-28', '2023-09-27', 2, 4, 2, 'Japanese', 'sushma', '2023-09-10'),
+    (11, 111, 2016, '2023-09-30', '2023-09-29', 3, 5, 4, 'Kannada', 'Nithin', '2023-09-11'),
+    (12, 112, 2020, '2023-10-02', '2023-10-01', 1, 4, 6, 'German', 'Rohith', '2023-09-12'),
+    (13, 113, 2007, '2023-10-04', '2023-10-03', 4, 3, 3, 'Tamil', 'mahantesh', '2023-09-13'),
+    (14, 114, 2013, '2023-10-06', '2023-10-05', 2, 5, 5, 'Spanish', 'Sugamma', '2023-09-14'),
+    (15, 115, 2018, '2023-10-08', '2023-10-07', 3, 4, 2, 'Urdu', 'Shivshankar', '2023-09-15'),
+    (16, 116, 2022, '2023-10-10', '2023-10-09', 1, 5, 7, 'German', 'Rangaswamy', '2023-09-16'),
+    (17, 117, 2009, '2023-10-12', '2023-10-11', 4, 3, 4, 'English', 'Savithra', '2023-09-17'),
+    (18, 118, 2014, '2023-10-14', '2023-10-13', 2, 4, 2, 'Spanish', 'Chandrakala', '2023-09-18'),
+    (19, 119, 2019, '2023-10-16', '2023-10-15', 3, 5, 6, 'French', 'BheemRao', '2023-09-19'),
+    (20, 120, 2023, '2023-10-18', '2023-10-17', 1, 4, 3, 'German', 'Abhi', '2023-09-20');
+    select * from library_info;
+    
+INSERT INTO TV_Info VALUES
+    (1, 'Samsung', 799, 55, 'SmartTV', 'Black', 45, 2, 3, 4, '4K Ultra HD'),
+    (2, 'LG', 699, 50, 'LGc2', 'Silver', 42, 2, 2, 3, 'Full HD'),
+    (3, 'Sony', 899, 65, 'S93', 'Black', 60, 3, 4, 4, '4K Ultra HD'),
+    (4, 'Panasonic', 599, 43, 'M-series', 'White', 38, 2, 2, 3, 'Full HD'),
+    (5, 'MI', 1199, 75, 'XRA95K', 'Black', 80, 3, 4, 4, '8K Ultra HD'),
+    (6, 'Hp', 499, 32, 'TV6', 'Black', 28, 1, 1, 2, 'HD Ready'),
+    (7, 'Google', 699, 49, 'OLED TV', 'Silver', 44, 2, 3, 3, '4K Ultra HD'),
+    (8, 'Oneplus', 799, 55, 'LG C3', 'Black', 46, 2, 3, 4, '4K Ultra HD'),
+    (9, 'Samsung', 599, 43, 'TV omni', 'White', 36, 1, 2, 2, 'Full HD'),
+    (10, 'LG', 1299, 77, 'LG C4', 'Silver', 85, 3, 4, 4, '8K Ultra HD'),
+    (11, 'TCL', 899, 65, 'U8K', 'Black', 63, 2, 3, 3, '4K Ultra HD'),
+    (12, 'VIZIO', 699, 49, 'U6K', 'Black', 45, 2, 2, 3, '4K Ultra HD'),
+    (13, 'Samsung', 599, 43, 'TV13', 'Silver', 40, 1, 2, 2, 'Full HD'),
+    (14, 'LG', 999, 60, 'SmartTV14', 'Black', 55, 2, 3, 3, '4K Ultra HD'),
+    (15, 'Sony', 899, 65, 'SmartTV15', 'Silver', 60, 2, 3, 3, '4K Ultra HD'),
+    (16, 'GoogleTv', 799, 55, 'SmartTV16', 'Black', 50, 2, 3, 4, '4K Ultra HD'),
+    (17, 'Samsung', 499, 32, 'QLED', 'Black', 28, 1, 2, 2, 'HD Ready'),
+    (18, 'LG', 799, 55, 'XRA95K', 'Silver', 47, 2, 3, 4, '4K Ultra HD'),
+    (19, 'crop', 699, 49, 'SmartTV19', 'Black', 42, 2, 2, 3, '4K Ultra HD'),
+    (20, 'LG Electronics', 599, 43, 'Quantum', 'Black', 36, 1, 2, 2, 'Full HD');
+
+INSERT INTO Cricket_Info1 VALUES
+    (1, 'Dhoni', 2000, 50, 90, 45, 5, 85, 3, 100, 'Batsman'),
+    (2, 'Kholi', 1500, 30, 85, 60, 2, 78, 1, 80, 'Bowler'),
+    (3, 'Rohith', 2500, 20, 95, 55, 4, 92, 5, 120, 'AllRounder'),
+    (4, 'Pandya', 1800, 40, 88, 65, 3, 80, 2, 90, 'Batsman'),
+    (5, 'Sachin', 2800, 25, 92, 50, 6, 95, 7, 150, 'Captain'),
+    (6, 'Bumrah', 2100, 35, 89, 60, 4, 88, 3, 110, 'AllRounder'),
+    (7, 'Jadeja', 2200, 30, 91, 58, 5, 90, 4, 120, 'Batsman'),
+    (8, 'Anil', 1400, 45, 85, 70, 2, 75, 0, 70, 'Bowler'),
+    (9, 'siraj', 2600, 15, 94, 52, 7, 92, 6, 140, 'AllRounder'),
+    (10, 'Kl Rahul', 1700, 35, 87, 62, 3, 82, 2, 100, 'Batsman'),
+    (11, 'Shreyas', 2400, 25, 93, 55, 6, 94, 5, 130, 'Bowler'),
+    (12, 'Pollard', 1900, 30, 88, 58, 4, 85, 1, 80, 'AllRounder'),
+    (13, 'Shami', 2300, 20, 92, 60, 5, 91, 3, 110, 'Batsman'),
+    (14, 'Kapil yadav', 1300, 50, 84, 72, 1, 72, 0, 60, 'Bowler'),
+    (15, 'Agarkar', 2700, 10, 95, 48, 8, 97, 8, 160, 'AllRounder'),
+    (16, 'Shardul', 2000, 40, 89, 63, 4, 86, 2, 100, 'Batsman'),
+    (17, 'Axar Patel', 2500, 25, 94, 56, 6, 92, 6, 140, 'Bowler'),
+    (18, 'Ajay', 1600, 30, 86, 61, 2, 79, 1, 70, 'AllRounder'),
+    (19, 'Chethan', 2200, 30, 91, 58, 5, 89, 4, 120, 'Batsman'),
+    (20, 'Sharma', 1200, 60, 83, 75, 1, 70, 0, 50, 'Bowler');
+    select * from cricket_info1;
